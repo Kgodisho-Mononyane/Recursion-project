8543a7b941a099d628be06dc609b811065b9d442
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2026-04-25 11:13:32 +0200

    complete fibonacci.js

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,33 +1,34 @@
 export function fibs(num) {
-    let array = []
-    for (let i = 0; i < num; i++) {
+    if (num <= 0) return [];
+    if (num === 1) return [0];
 
+    let array = [0, 1];
+
+    for (let i = 2; i < num; i++) {
+        array.push(array[i - 1] + array[i - 2]);
     }
-    // Using iteration, write a function fibs which takes a 
-    // number and returns an array containing that 
-    // many numbers from the Fibonacci sequence.
     return array;
 }
 
+console.log(fibs(8))
 
 export function fibsRec(num) {
     if (num == 0) {
         return [];
-    } else if (num == 1) {
-        return [1];
     } else {
         let array = [];
 
-        for (let i = 0; i <= num; i++) {
-            let fib = fibsRec(num = 1) + fibsRec(num - 2); {
-                array.push(fib)
-            } 
+        function fibonacci(n) {
+            if (n < 0) return undefined;
+            if (n < 2) return n;
+            return fibonacci(n - 1) + fibonacci(n - 2)
         }
 
+        for (let i = 0; i < num; i++) {
+            array.push(fibonacci(i))
+        }
         return array;
     }
 }
-
-console.log(fibsRec(5))
 
 //module.exports = {fibs, fibsRec};
--- tests/fibonacci.test.js
+++ tests/fibonacci.test.js
@@ -5,19 +5,19 @@ test('iterative fibonacci function works with 0', () => {
 })
 
 test('iterative fibonacci function works with 1', () => {
-    expect(fibs(1)).toEqual([1]);
+    expect(fibs(1)).toEqual([0]);
 })
 
 test('iterative fibonacci function works with 2', () => {
-    expect(fibs(2)).toEqual([1,1]);
+    expect(fibs(2)).toEqual([0,1]);
 })
 
 test('iterative fibonacci function works with 5', () => {
-    expect(fibs(5)).toEqual([1,1,2,3,5]);
+    expect(fibs(5)).toEqual([0,1,1,2,3]);
 })
 
 test('iterative fibonacci function works with 8', () => {
-    expect(fibs(8)).toEqual([1,1,2,3,5,8,13,21]);
+    expect(fibs(8)).toEqual([0,1,1,2,3,5,8,13]);
 })
 
 test('recursive fibonacci works with 0', () => {
@@ -25,17 +25,17 @@ test('recursive fibonacci works with 0', () => {
 })
 
 test('recursive fibonacci works with 1', () => {
-    expect(fibsRec(1)).toEqual([1]);  
+    expect(fibsRec(1)).toEqual([0]);  
 })
 
 test('recursive fibonacci works with 2', () => {
-    expect(fibsRec(2)).toEqual([1,1]);  
+    expect(fibsRec(2)).toEqual([0,1]);  
 })
 
 test('recursive fibonacci works with 5', () => {
-    expect(fibsRec(5)).toEqual([1,1,2,3,5]);  
+    expect(fibsRec(5)).toEqual([0,1,1,2,3]);  
 })
 
 test('recursive fibonacci works with 8', () => {
-    expect(fibsRec(8)).toEqual([1,1,2,3,5,8,13,21]);  
+    expect(fibsRec(8)).toEqual([0,1,1,2,3,5,8,13]);  
 })
